9762fde3e60a8dc94ba047341198235f00ae59ef
KwangwonChoi <rhkddnjs99@hotmail.com>
2019-08-21 21:39:01 +0900

    Update ckw.py

--- Programmers/190821[1차]뉴스클러스터링/ckw.py
+++ Programmers/190821[1차]뉴스클러스터링/ckw.py
@@ -34,3 +34,38 @@ def solution(str1, str2):
     
     ans = intersection / union * 65536
     return int(ans)
+
+
+
+// 제일 좋은 코드 (아마?)
+
+def get_intersection_num(list1, list2):
+
+    intersection = set(list1) & set(list2)
+    union = set(list1) | set(list2)
+
+    intersection_len = sum([min(list1.count(e), list2.count(e)) for e in intersection])
+    union_len = sum([max(list1.count(e), list2.count(e)) for e in union])
+
+    return intersection_len, union_len
+
+
+def make_list(str):
+    return [
+        (str[i:i+2]).lower()
+        for i in range(0, len(str) - 1)
+        if str[i:i+2].isalpha()
+    ]
+
+
+def solution(str1, str2):
+    list1 = make_list(str1)
+    list2 = make_list(str2)
+
+    intersection, union = get_intersection_num(list1, list2)
+
+    if union == 0:
+        return 65536
+
+    ans = intersection / union * 65536
+    return int(ans)
