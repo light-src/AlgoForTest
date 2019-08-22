a92a96b6aa88439daf636c8f196593a9ee78353c
이태영 <taeyounglee93@gmail.com>
2019-08-22 17:22:25 +0900

    🔥🔥🔥🔥🔥🔥🔥🔥

--- Programmers/190822[1차]추석트래픽/tyl.py
+++ Programmers/190822[1차]추석트래픽/tyl.py
@@ -5,8 +5,7 @@ def solution(lines):
     logs, ret = [], 0
 
     for s in lines:
-        end = int(s[11:13])*60*60 + int(s[14:16])*60 + \
-            int(s[17:19]) + float('0.'+s[20:23])
+        end = int(s[11:13])*60*60 + int(s[14:16])*60 + int(s[17:19]) + float('0.'+s[20:23])
         start = round(end - float(s[24:-1]) + 0.001, 3)
         logs.append([start, end])
 
@@ -15,10 +14,8 @@ def solution(lines):
         start, end = log[1], round(log[1]+0.999, 3)
         for others in logs:
             first, last = map(float, others)
-            if first <= start and last >= start:
-                count += 1
-            elif start <= first <= end:
-                count += 1
+            if first <= start and last >= start: count += 1
+            elif start <= first <= end: count += 1
 
         ret = max(count, ret)
     return ret
