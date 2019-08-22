497f43e7b7901a6fedaa748fac09914dd7cb22dd
이태영 <taeyounglee93@gmail.com>
2019-08-22 17:15:46 +0900

    🔥🔥🔥🔥🔥🔥🔥🔥

---
+++ Programmers/190822[1차]추석트래픽/tyl.py
@@ -0,0 +1,25 @@
+# kakao_holiday_traffic
+
+def solution(lines):
+    logs,ret = [],0
+    #if len(lines) == 1: return 1
+    for s in lines:        
+        end = int(s[11:13])*60*60 + int(s[14:16])*60 + int(s[17:19]) + float('0.'+s[20:23])
+        start = round(end - float(s[24:-1]) + 0.001,3)
+        logs.append([start,end])
+    logs.sort()
+    
+    for log in logs:
+        count = 0
+        start, end = log[1], round(log[1]+0.999,3)
+        for others in logs:
+            first, last = map(float, others)
+            if first <= start and last >= start : count += 1
+            elif start <= first <= end: count+= 1
+                
+        ret = max(count, ret)
+    return ret
+    
+if __name__ == "__main__":
+    lines =	["2016-09-15 01:00:04.002 2.0s", "2016-09-15 01:00:07.000 2s"]
+    print(solution(lines))    
